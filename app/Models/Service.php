e0f98175ca564e5168a0aae63cf1a3fe8bddacf4
Hirakhizar <hirakhizarkhizarhayat@gmail.com>
2024-11-01 05:00:22 -0700

    merging

--- app/Models/Service.php
+++ app/Models/Service.php
@@ -15,4 +15,26 @@ class Service extends Model
             'status',
             'category_id'
     ];
+
+    public function carts()
+    {
+        return $this->hasMany(Cart::class);
+    }
+    public function category()
+    {
+        return $this->belongsTo(ServiceCategory::class);
+    }
+    public function cartItems()
+    {
+        return $this->hasMany(CartItems::class);
+    }
+    public function deal()
+    {
+        return $this->hasMany(Deal::class);
+    }
+    public function dealService()
+    {
+        return $this->hasMany(DealService::class, 'deal_service_id');
+
+    }
 }
